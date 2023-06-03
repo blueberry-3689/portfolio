d65c04dd2c117cbc58a3a1c15244ea7404b3a118
blueberry <blueberry.21.peach@gmail.com>
2023-06-03 19:33:29 +0900

    portfolio

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function (){
+    console.log("ok!")
+})
--- javascript/top.js
+++ javascript/top.js
@@ -3,22 +3,51 @@ $(function(){
     var count = $("#slide li").length;
     var current = 1;
     var next = 2;
-    var interval = 3000;
-    var duration = 500;
+    var interval = 5000;
+    var duration = 700;
     var timer;
     $("#slide li:not(:first-child)").hide();
     timer = setInterval(slideTimer, interval);
     function slideTimer(){
         $("#slide li:nth-child(" + current + ")").fadeOut(duration);
         $("#slide li:nth-child(" + next + ")").fadeIn(duration);
+        current = next;
+        next= ++next;
+        if(next > count){
+            next = 1;
+        }
+
+        //targetクラス削除
+        $("#button li a").removeClass("target");
+        //現在のボタンにtargetクラスを追加
+        $("#button li:nth-chuld("+ current +") a")
+        .addClass("target");
     }
+    //スライドショー　ボタン
+    $("#button li a").click(function(){
+        next = $(this).html();
+        clearInterval(timer);
+        timer = setInterval(slideTimer , interval);
+        slideTimer();
+        return false;
+    });
 });
 
 
 
+
+
 //トップメッセージのアニメーション
-const message = document.querySelector('#top');
-const keyfames ={
-    opacity: [0,1]
+const showmessage =(entries) => {
+    const keyframes ={
+        opacity: [0,1],
+        translate: ['0 100px', 0],
+    };
+    entries[0].target.animate(keyframes, 600);
 };
-message.animate (keyfames,2000);
+
+//監視ロボットの設定
+const messageObserber = new IntersectionObserver(showmessage);
+
+//#messageを監視するように指示
+messageObserber.observe(document.querySelector('#message'));
